44cf51559df2461f1b5b4686d690e2c513003634
tecrcopentgit <abdulhaadhi015@gmail.com>
2025-08-27 21:29:33 +0530

    server.js edited

--- pda_pro/staticfiles/pda_app/js/server.js
+++ pda_pro/staticfiles/pda_app/js/server.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin:allowedOrigins,
   credentials: true
 }));
 app.use(express.json());
