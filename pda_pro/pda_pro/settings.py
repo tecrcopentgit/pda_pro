34be826b4dea63f04cbb627c5a78ba5f29f05d46
tecrcopentgit <abdulhaadhi015@gmail.com>
2025-08-27 23:41:22 +0530

    static added

--- pda_pro/pda_pro/settings.py
+++ pda_pro/pda_pro/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'pda_app/static',
+    os.path.join(BASE_DIR,'static')
 ]
 
 
